5a9b91335b63eaec2b2ae948f7a0de0c82ac52eb
proj-int01 <adilson.monte.alto@gmail.com>
2022-07-14 06:24:53 -0300

    created ProdutoModelForm

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 #from pyexpat.errors import messages
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import ContatoForm
+from .forms import ContatoForm, ProdutoModelForm
 
 def index(request):
     return render(request, 'index.html')
@@ -26,5 +26,24 @@ def contato(request):
 
 
 def produto(request):
-    return render(request, 'produto.html')
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+            
+            print(f'Nome: {prod.nome}')
+            print(f'Preço: {prod.preco}')
+            print(f'Estoque: {prod.estoque}')
+
+            messages.success(request, 'Produto salvo com sucesso.')
+            form = ProdutoModelForm() # limpa as entradas do form na página
+        else:
+            messages.error(request, 'Erro ao salvar o produto.')
+
+    else:
+        form = ProdutoModelForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'produto.html', context )
 
